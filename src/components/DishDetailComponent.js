f5508ee2144fcea1601ec422d5ab91e1d2f7cbfe
Samiya Kazi <samiya.kazi@northsouth.edu>
2020-12-17 18:31:50 +0600

    Presentational and Container Components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -30,7 +30,7 @@ class DishDetail extends Component {
                     return(
                         <li>
                             <p>{comment.comment}</p>
-                            <p>--{comment.author}, {comment.date}</p>
+                            <p>--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                         </li>
                     );
                 })
